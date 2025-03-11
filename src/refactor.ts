e635d6e1d53c71766f5f8168ee2cafb8d5a39993
udbhavbalaji <udbhavbalaji@gmail.com>
2025-03-10 21:25:59 -0400

    syncing changes from mac mini to complete error-logger

--- src/refactor.ts
+++ src/refactor.ts
@@ -7,10 +7,17 @@ type ReturnTypes =
   | number
   | boolean
   | void;
-type ErrorHandler<T extends ReturnTypes = ReturnTypes> = (err: Error) => T;
+
+type ErrorHandler<
+  H extends ErrorReturnMapping,
+  T extends ReturnTypes = ReturnTypes,
+> = (err: RegError<H>) => T;
+type RegError<T extends ErrorReturnMapping, E extends Error = Error> = E & {
+  name: keyof T;
+};
 type ErrorReturnMapping = Record<string, ReturnTypes>;
 type ErrorHandlerMapping<T extends ErrorReturnMapping> = {
-  [K in keyof T]: ErrorHandler<T[K]>;
+  [K in keyof T]: ErrorHandler<T, T[K]>;
 };
 
 class LogErr<Mapping extends ErrorReturnMapping> {
@@ -18,8 +25,8 @@ class LogErr<Mapping extends ErrorReturnMapping> {
   private handlers: ErrorHandlerMapping<Mapping>;
 
   constructor(handlers: ErrorHandlerMapping<Mapping>) {
-    this.handlers = handlers;
     this.logger = createLogifier({ level: "error", context: "ErrorLog" });
+    this.handlers = handlers;
   }
 
   private formContextAndMessage(err: Error): {
@@ -35,7 +42,7 @@ class LogErr<Mapping extends ErrorReturnMapping> {
     const handler = this.handlers[errorName];
     if (!handler) throw new Error("No handler");
     try {
-      return handler(err) as any;
+      return handler(err);
     } catch (err) {
       throw new Error("Error in handler");
     }
@@ -46,29 +53,30 @@ class LogErr<Mapping extends ErrorReturnMapping> {
     this.logger.overrideContext(context).error(message);
   }
 
-  logErrorToFile(err: Error): void {
-    const { context, message } = this.formContextAndMessage(err);
-    this.logger.overrideContext(context).errorToFile(message);
+  private isHandleableError(
+    name: string,
+  ): name is Extract<keyof Mapping, string> {
+    return name in this.handlers;
   }
 
-  logAndHandleError<T = unknown>(err: Error) {
-    this.logError(err);
-    return this.handleError(err.name, err) as T;
+  // logAndHandleError<K extends keyof typeof this.handlers>(
+  //   err: Error & { name: K },
+  // ): ReturnType<(typeof this.handlers)[K]> {
+  //   this.logError(err);
+  //   return this.handleError(err.name, err) as any;
+  // }
+  logAndHandleError<E extends Error>(err: RegError<Mapping, E>) {
+    if (this.isHandleableError(err.name)) {
+      this.logError(err);
+      return this.handleError(err.name, err);
+    } else {
+      return;
+    }
   }
-
-  logAndHandleErrorToFile<T = unknown>(err: Error) {
-    this.logErrorToFile(err);
-    return this.handleError(err.name, err) as T;
-  }
-}
-
-function createLogErr<Mapping extends ErrorReturnMapping>(
-  handlers: ErrorHandlerMapping<Mapping>,
-): LogErr<Mapping> {
-  return new LogErr(handlers);
 }
 
 class CA extends Error {
+  name: "CA";
   constructor(msg: string) {
     super(msg);
     this.name = "CA";
@@ -83,8 +91,10 @@ class CB extends Error {
 }
 
 const logerr = new LogErr({
-  CA: (err: CA) => console.log(err.name),
+  CA: (err: Error) => console.log(err.name),
   CB: (err: CB) => err.name,
 });
+
+const err = new CA("this is CA");
 
 const res = logerr.logAndHandleError(new CA("This is CA"));
